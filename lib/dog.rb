962e44740fc80c8e49796b3a672bbf795027a921
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-11-14 21:04:28 -0800

    commit tree 39b9ff69458c82bc0c19387e4d4acca2ccb8ae1a

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  # class body
+
+  # Instance method definition
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
